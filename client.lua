2a24dd2116ede89c62b8c3df7468b19fd35a3fb3
powerboat9 <powerboat9@users.noreply.github.com>
2016-01-10 12:23:37 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,3 +10,16 @@ end
 function validColor(c)
     return equalToOr(c, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768)
 end
+
+function createNewViewport()
+    return {
+        ["matrix"] = {},
+        ["newColoum"] = function(self)
+            self.matrix[#self.matrix + 1] = {}
+        end
+        ["makeColoums"] = function(self, n)
+            for i = 1, n do
+                self:newColoum()
+            end
+        end
+        ["
